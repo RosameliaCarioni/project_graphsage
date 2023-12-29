c0a963888350aebf7501dafd0c58c49dc583259d
rosameliacarioni <r.carioniporras@student.maastrichtuniversity.nl>
2023-12-29 22:24:47 +0000

    crying

--- graphsage_experiments.py
+++ graphsage_experiments.py
@@ -8,8 +8,27 @@ from torch_geometric.loader import LinkNeighborLoader
 
 from sklearn.model_selection import StratifiedKFold, cross_validate
 
+# This work is inspired/based in the following work: 
+
+# https://github.com/pyg-team/pytorch_geometric/blob/master/examples/ogbn_products_sage.py
+# https://github.com/PacktPublishing/Hands-On-Graph-Neural-Networks-Using-Python/blob/main/Chapter08/chapter8.ipynb 
+# https://github.com/pyg-team/pytorch_geometric/blob/master/examples/graph_sage_unsup.py
+# https://pytorch-geometric.readthedocs.io/en/latest/generated/torch_geometric.nn.conv.SAGEConv.html
+# https://medium.com/@juyi.lin/neighborloader-introduction-ccb870cc7294
 
 def train(model, device, train_loader, optimizer, number_nodes):
+    """_summary_
+
+    Args:
+        model: _description_
+        device: _description_
+        train_loader: _description_
+        optimizer: _description_
+        number_nodes: _description_
+
+    Returns:
+        _type_: _description_
+    """    
     model.train()
     total_loss = 0
     for batch in train_loader:
@@ -32,6 +51,15 @@ def train(model, device, train_loader, optimizer, number_nodes):
 
 @torch.no_grad()
 def test_node_classification(embedding_matrix, y):
+    """_summary_
+
+    Args:
+        embedding_matrix: _description_
+        y: _description_
+
+    Returns:
+        _type_: _description_
+    """    
     model = LogisticRegression(multi_class="ovr")
     kf = StratifiedKFold(n_splits=5, shuffle=True, random_state=123)
     eval_scores = {"acc": "accuracy", "f1_macro": "f1_macro", "f1_micro": "f1_micro"}
@@ -52,7 +80,6 @@ def test_link_prediction(embedding_matrix, edge_label_index, y):
 
     kf = StratifiedKFold(n_splits=5, shuffle=True, random_state=123)
     eval_scores = {"acc": "accuracy", "roc_auc": "roc_auc"}
-    
 
     # TODO 
     return 0
@@ -63,6 +90,7 @@ def compute_embedding_matrix(
     number_features,
     number_nodes,
     batch_size,
+    hidden_layer, 
     epochs, 
     neighborhood_1,
     neighborhood_2,
@@ -75,6 +103,29 @@ def compute_embedding_matrix(
     bias,
     normalize
 ):
+    """_summary_
+
+    Args:
+        data (_type_): _description_
+        number_features (_type_): _description_
+        number_nodes (_type_): _description_
+        batch_size (_type_): _description_
+        hidden_layer (_type_): _description_
+        epochs (_type_): _description_
+        neighborhood_1 (_type_): _description_
+        neighborhood_2 (_type_): _description_
+        embedding_dimension (_type_): _description_
+        learning_rate (_type_): _description_
+        dropout_rate (_type_): _description_
+        activation_function (_type_): _description_
+        aggregator (_type_): _description_
+        activation_before_normalization (_type_): _description_
+        bias (_type_): _description_
+        normalize (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """    
     # Sampling from neighbourhood
     train_loader = LinkNeighborLoader(
         data,
@@ -93,7 +144,7 @@ def compute_embedding_matrix(
     # Create model
     model = GraphSAGE(
         in_channels=number_features,
-        hidden_channels=number_features,  # TODO: not sure
+        hidden_channels=hidden_layer,  # TODO: not sure
         out_channels=embedding_dimension,
         num_layers=2,
         aggr=aggregator,
@@ -115,15 +166,18 @@ def compute_embedding_matrix(
         model.eval()
         embedding_matrix = model(data.x, data.edge_index).to(device)
 
+        # EVALUATE NODE CLASSIFICATION
         y = data.y
         acc, f1_macro, f1_micro = test_node_classification(embedding_matrix, y)
 
-
-
+        print('Node classification ')
         print(
             f"Epoch: {epoch:03d}, Accuracy: {acc:.4f}, "
             f"Total loss: {total_loss:.4f}, f1_macro: {f1_macro:.4f}, f1_micro:{f1_micro:.4f} "
         )
+
+        # EVALUATE LINK PREDICTION TODO 
+
         times.append(time.time() - start)
     print(f"Median time per epoch: {torch.tensor(times).median():.4f}s")
 
