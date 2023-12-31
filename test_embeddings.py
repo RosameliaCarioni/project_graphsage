a547b355e99f0afbfb4e648d018706a66e060b77
rosameliacarioni <r.carioniporras@student.maastrichtuniversity.nl>
2023-12-31 14:39:21 +0000

    pub med experiments done

--- test_embeddings.py
+++ test_embeddings.py
@@ -9,7 +9,9 @@ from sklearn.multiclass import OneVsRestClassifier
 from sklearn.model_selection import StratifiedKFold, KFold, cross_validate, train_test_split
 from sklearn.metrics import roc_auc_score
 from itertools import combinations
-
+from torch_geometric.transforms import RandomLinkSplit
+import numpy as np
+from sklearn.metrics import roc_auc_score
 
 # ---------------------------------------------- NODE CLASSIFICATION TASK --------------------------------------------
 
@@ -56,8 +58,69 @@ def test_node_classification_multi_class(embedding_matrix, y, n_folds=5):
     acc, f1_macro, f1_micro = results['test_acc'].mean(), results['test_f1_macro'].mean(), results['test_f1_micro'].mean()
     return acc, f1_macro, f1_micro
 
+# ---------------------------------------------- LINK PREDICTION TASK: ERIC's IMPLEMENTATION   --------------------------------------------
 
-# ---------------------------------------------- LINK PREDICTION TASK --------------------------------------------
+def train_test_split_graph(data, is_undirected = True):
+    """
+    Splits a Graph into a test and train set randomly to 80-20. The test split is balanced with negative edges sampled from random vertex pairs that have no edges between them. 
+    While removing edges randomly, it makes sure that no vertex is isolated.
+    :param data: a torch geometrics graph to be split
+    :return: the train-test split as torch geometrics graphs
+    """
+    
+    transform = RandomLinkSplit(num_val=0, num_test=0.2, is_undirected = is_undirected, add_negative_train_samples=False)
+    train_data, _, test_data = transform(data)
+    return train_data, test_data
+
+def predict_link(u, v, embeddings):
+    """
+    Computes the normalized probability for an existing link between two nodes u and v based on the input
+    embeddings.
+    :param u: a node in the graph
+    :param v: a node in the graph
+    :param embeddings: trained embeddings
+    :return: sigmoid normalized probability for the existence of a link
+    """
+    embedding1 = embeddings[u]
+    embedding2 = embeddings[v]
+    
+    # Compute inner product (dot product)
+    dot_product = np.dot(embedding1, embedding2)
+
+    # Normalize by sigmoid function
+    link_probability = 1/(1 + np.exp(-dot_product))
+    return link_probability
+
+def link_predictions(embeddings, edges, y_true):
+    """
+    Computes the ROC-AUC score for a given set of test edges based on the trained embeddings.
+    :param embeddings: a models trained embeddings
+    :param edges: test edges
+    :param y_true: the labels for edges (1=true, 0=false)
+    :return: the ROC-AUC score from predictions
+    """
+    predictions = []
+    for edge in edges:
+        predictions.append(predict_link(edge[0], edge[1], embeddings))
+    return roc_auc_score(y_true, predictions) 
+
+def k_fold_cross_validation_link_prediction(embeddings, edges, y_true, k=5):
+    kf = KFold(n_splits=k, shuffle=True)
+    auc_scores = []
+
+    for train_index, test_index in kf.split(edges):
+        # Splitting the data into training and test sets
+        _, test_edges = edges[train_index], edges[test_index]
+        _, y_test = y_true[train_index], y_true[test_index]
+
+        # Evaluating the model on the test set
+        score = link_predictions(embeddings, test_edges, y_test)
+        auc_scores.append(score)
+
+    return np.mean(auc_scores) 
+
+
+# ---------------------------------------------- LINK PREDICTION TASK: JACK's IMPLEMENTATION + Auxiliary methods  --------------------------------------------
 
 @torch.no_grad()
 def test_link_prediction_k_fold_validation(embedding, edges, non_edges, k=5):
@@ -159,14 +222,11 @@ def get_edges_and_non_edges_as_lists(data, directed):
 
     # Get lists of nodes that form an edge
     edge_index = data.edge_index
-    print('done with edge_index')
     # make edges a list with 2 elements per entry 
     edges = list(zip(edge_index[0].numpy(), edge_index[1].numpy()))
-    print('done with edges')
 
     # Generate a set with all possible combinations of existing edges
     all_possible_pairs = set(combinations(range(data.num_nodes), 2))    
-    print('done with all_possible_pairs')
 
     # Set edges_exist based on whether the graph is directed or not
     if directed: 
@@ -175,10 +235,8 @@ def get_edges_and_non_edges_as_lists(data, directed):
     else:
         # For undirected graphs, sort the nodes in each edge
         edges_exist = list(set(map(lambda e: tuple(sorted(e)), edges)))
-    print('done with directed/non directed')
 
     # Subtract from all possible pairs, the ones that already exist
     non_edges = list(all_possible_pairs - set(edges_exist))
-    print('done with non_edges')
 
     return edges, non_edges
